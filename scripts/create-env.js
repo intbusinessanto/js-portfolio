44d7ba62641412bdff31ccd0a874cb102a524681
root <root@SANTY-PC.localdomain>
2022-03-10 14:18:32 -0500

    first commit

---
+++ scripts/create-env.js
@@ -0,0 +1,2 @@
+const fs = require('fs');
+fs.writeFileSync('./.env', `ÀPI=${process.env.API}\n`)
---
+++ src/index.js
@@ -0,0 +1,9 @@
+import Template from '@templates/Template.js';
+import '@styles/main.css';
+import '@styles/vars.styl';
+console.log('Hola Care Bola');
+
+(async function App() {
+    const main = null || document.getElementById('main');
+    main.innerHTML = await Template();
+})();
